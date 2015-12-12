20981b684cbe6b8d42bf26165d670314e9301ad0
Markus Hiesmair <markus.hiesmair@yahoo.de>
2015-12-12 12:21:32 +0100

    added simple analysis measures

---
+++ politician_time_national_council.sql
@@ -0,0 +1,5 @@
+﻿select politician_id, sum(age(coalesce(valid_until, current_date), valid_from)) from mandate
+where mandate_type = 'NationalCouncilMember'
+group by politician_id
+order by sum(age(coalesce(valid_until, current_date), valid_from)) desc
+
